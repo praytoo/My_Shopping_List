2aeb4ac35d09a0339959e9f098acaf15518a0be8
praytoo <praytoo52@icloud.com>
2025-09-22 15:10:12 -0700

    My Shopping List for LTCA

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Shopping List");
+        System.out.println("Apricot");
+        System.out.println("Apple");
+        System.out.println("Watermelon");
+        System.out.println("Banana");
+        System.out.println("Pineapple");
+        System.out.println("Broccoli");
+        System.out.println("Asparagus");
+        System.out.println("Cherry");
+        System.out.println("Strawberry");
+        System.out.println("Onion");
+    }
+}
